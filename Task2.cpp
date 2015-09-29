7b5f99fc1cdf47625a6f92fda4840c5b7b1e4b71
Alex <alexeyotutski@gmail.com>
2015-09-29 10:51:59 +0300

    Added tests for Task2

--- Task2.cpp
+++ Task2.cpp
@@ -12,7 +12,7 @@ int* max_multiply(int* array, int n)
 	{
 		res[0] = array[n - 1];
 		res[1] = array[n - 2];
-		res[2] = array[n-3];
+		res[2] = array[n - 3];
 	}
 	if (array[0] > 0)
 	{
@@ -36,22 +36,46 @@ int* max_multiply(int* array, int n)
 
 }
 
+void testitself(int* ar, int n, int* aranswer,int &j)
+{
+	bool right=1;
+	int* result = max_multiply(ar, 10);
+	for (int i=0;i<3;i++)
+	{
+		if(result[i]!=aranswer[i])
+			right=0;
+	}
+	if (right)
+		cout<<"Test "<<j<<" work propelry"<<endl;
+	else
+		cout<<"Test "<<j<<" work with mistakes."<<endl;
+	j++;
+}
+
+void test()
+{
+	int j=1;
+	int ar1[10]={1,2,3,4,5,6,7,8,9};
+	int n1=9;
+	int ar1answer[3]={9,8,7};
+	testitself(ar1,n1,ar1answer,j);
+	int ar2[10]={10,20,40,50,44,100};
+	int n2=6;
+	int ar2answer[3]={100,50,44};
+	testitself(ar2,n2,ar2answer,j);
+	int ar3[10]={100,501,99,1,2,3,20};
+	int n3=7;
+	int ar3answer[3]={501,100,99};
+	testitself(ar3,n3,ar3answer,j);
+	int ar4[10]={10,12,13,14,20,29,1,10};
+	int n4=8;
+	int ar4answer[3]={29,20,14};
+	testitself(ar4,n4,ar4answer,j);
+}	
+
 int main()
 {
-	int *array;
-	int n;
-	cout << "Enter the array" << endl;
-	cin >> n;
-	array = new int[n];
-	for (int i = 0; i < n; i++)
-	{
-		cin >> array[i];
-	}
-	int* result = max_multiply(array, n);
-	cout << "The answer is" << endl;
-	for (int i = 0; i < 3; i++)
-		cout << result[i] << " ";
-	cout << endl;
+	test();
 	getchar();
 	getchar();
 	return 0;
