8fa26a30a8a62554d5e3285932f854594cfa1de7
Pashtet32 <pavelt0309@gmail.com>
2015-09-30 16:21:08 +0200

    final version task 2 with tests

--- Task2.cpp
+++ Task2.cpp
@@ -10,16 +10,10 @@ int* max_multiply(int* array, int n)
 	sort(array, array + n);
 	if (array[n - 1] <= 0)
 	{
-<<<<<<< HEAD
-		res[0] = array[n-1];
-		res[1] = array[n-2];
-		res[2] = array[n-3];
-		return res;
-=======
 		res[0] = array[n - 1];
 		res[1] = array[n - 2];
 		res[2] = array[n - 3];
->>>>>>> origin/master
+		return res;
 	}
 	if (array[0] > 0)
 	{
@@ -44,42 +38,42 @@ int* max_multiply(int* array, int n)
 
 }
 
-void testitself(int* ar, int n, int* aranswer,int &j)
+void testitself(int* ar, int n, int* aranswer, int &j)
 {
-	bool right=1;
+	bool right = 1;
 	int* result = max_multiply(ar, 10);
-	for (int i=0;i<3;i++)
+	for (int i = 0; i<3; i++)
 	{
-		if(result[i]!=aranswer[i])
-			right=0;
+		if (result[i] != aranswer[i])
+			right = 0;
 	}
 	if (right)
-		cout<<"Test "<<j<<" work propelry"<<endl;
+		cout << "Test " << j << " work propelry" << endl;
 	else
-		cout<<"Test "<<j<<" work with mistakes."<<endl;
+		cout << "Test " << j << " work with mistakes." << endl;
 	j++;
 }
 
 void test()
 {
-	int j=1;
-	int ar1[10]={1,2,3,4,5,6,7,8,9};
-	int n1=9;
-	int ar1answer[3]={9,8,7};
-	testitself(ar1,n1,ar1answer,j);
-	int ar2[10]={10,20,40,50,44,100};
-	int n2=6;
-	int ar2answer[3]={100,50,44};
-	testitself(ar2,n2,ar2answer,j);
-	int ar3[10]={100,501,99,1,2,3,20};
-	int n3=7;
-	int ar3answer[3]={501,100,99};
-	testitself(ar3,n3,ar3answer,j);
-	int ar4[10]={10,12,13,14,20,29,1,10};
-	int n4=8;
-	int ar4answer[3]={29,20,14};
-	testitself(ar4,n4,ar4answer,j);
-}	
+	int j = 1;
+	int ar1[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int n1 = 9;
+	int ar1answer[3] = { 9, 8, 7 };
+	testitself(ar1, n1, ar1answer, j);
+	int ar2[10] = { 10, 20, 40, 50, 44, 100 };
+	int n2 = 6;
+	int ar2answer[3] = { 100, 50, 44 };
+	testitself(ar2, n2, ar2answer, j);
+	int ar3[10] = { 100, 501, 99, 1, 2, 3, 20 };
+	int n3 = 7;
+	int ar3answer[3] = { 501, 100, 99 };
+	testitself(ar3, n3, ar3answer, j);
+	int ar4[10] = { 10, 12, 13, 14, 20, 29, 1, 10 };
+	int n4 = 8;
+	int ar4answer[3] = { 29, 20, 14 };
+	testitself(ar4, n4, ar4answer, j);
+}
 
 int main()
 {
